f01acf24157f0b3caddc52fa2488a74d6552cee0
Daniel Mar <dkmar@protonmail.com>
2024-01-12 16:20:20 -0800

    Preliminary effort towards appeasing the LSP with numpy typing.

---
+++ wordle/typing.py
@@ -0,0 +1,7 @@
+from collections.abc import Callable, Mapping, Iterable, Sequence
+from typing import Optional, Any
+
+import numpy as np
+import numpy.typing as npt
+
+WordIndexArray = npt.NDArray[np.int16]
