6bb48ff113303dca9444f5eed082eb692c2ca08f
Daniel Mar <dkmar@protonmail.com>
2024-01-20 09:37:02 -0800

    Moved types to typing.py

--- wordle/typing.py
+++
@@ -1,7 +0,0 @@
-from collections.abc import Callable, Mapping, Iterable, Sequence
-from typing import Optional, Any
-
-import numpy as np
-import numpy.typing as npt
-
-WordIndexArray = npt.NDArray[np.int16]
